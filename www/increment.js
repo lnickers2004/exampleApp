c87ae16396ebc9f26486dd8b42dbd602c381bd8f
Larry Nickerson <lnickers@austin.rr.com>
2015-04-08 14:42:20 -0500

    updated increment directive to use resource  Action

--- www/increment.js
+++ www/increment.js
@@ -6,7 +6,10 @@ angular.module('increment', [])
       // priority: 1,
       // terminal: true,
        scope: {
-         value:"=value"
+         item:"=item",
+         property:"@propertyName",
+         restful:"@restful",
+         method:"@methodName"
        }, // {} = isolate, true = child, false/undefined = no change
       // controller: function($scope, $element, $attrs, $transclude) {},
       // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
@@ -22,7 +25,11 @@ angular.module('increment', [])
         iElm.append(button);
         button.on("click", function () {
           $scope.$apply(function () {
-            $scope.value++;
+            $scope.item[$scope.property]++;
+            if ($scope.restful) {
+              // call the action to persist the canges to the server
+              $scope.item[$scope.method]();
+            }
           });
         });
       }
